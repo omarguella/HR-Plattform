c8f22b0215bc9fc29d8124b8f7bd1bd04de36b45
omarguella <83080774+omarguella@users.noreply.github.com>
2021-12-12 18:59:28 +0100

    usersEND

--- backend/src/apis/user-api.js
+++ backend/src/apis/user-api.js
@@ -1,9 +1,133 @@
+const userService = require('../services/user-service');
+const user = require("../models/User");
+
 /**
  * endpoint, which returns information about the user, which is currently authenticated
  * @param req express request
  * @param res express response
  * @return {Promise<void>}
  */
-exports.getSelf = async function(req, res){
+exports.getSelf = async function (req, res) {
     res.send(req.session.user); //retrieve userdata of authenticated user from session and return it
+}
+
+
+/**
+ * endpoint, which returns all users
+ * @param req express request
+ * @param res express response
+ * @return {Promise<void>}
+ */
+exports.getAll = async function (req, res) {
+    const db = req.app.get('db');
+    res.json(await userService.getAll(db));
+}
+
+
+/**
+ * endpoint, which returns an user by his username
+ * @param req express request
+ * @param res express response
+ * @return {Promise<void>}
+ */
+exports.getOne = async function (req, res) {
+    const db = req.app.get('db');
+    const username = req.params.username;
+    const user = await userService.get(db, username);
+
+    if (user == null) {
+        res.status(404).json({
+            message: 'user not found'
+        });
+    } else {
+        res.json(user);
+    }
+
+}
+
+
+/**
+ * endpoint, to create a User
+ * @param req express request
+ * @param res express response
+ * @return {Promise<void>}
+ */
+exports.create = async function (req, res) {
+    const db = req.app.get('db');
+    /* todo body validation */
+    const {
+        username,
+        firstname,
+        lastname,
+        email,
+        password,
+        isAdmin
+    } = req.body;
+
+    const newUser = new user(
+        username,
+        firstname,
+        lastname,
+        email,
+        password,
+        isAdmin);
+
+    const _u = await userService.get(db, newUser.username);
+
+    if (_u !== null) {
+        res.status(400).json({
+            message: 'User already exists'
+        });
+    } else {
+        await userService.add(db, newUser);
+        res.status(201).json(await userService.get(db, newUser.username))
+    }
+}
+
+
+/**
+ * endpoint, to update User by his unique username
+ * @param req express request
+ * @param res express response
+ * @return {Promise<void>}
+ */
+exports.update = async function (req, res) {
+    const username = req.params.username;
+    const db = req.app.get('db');
+    const updatedValues = req.body;
+    delete(updatedValues._id); // prevent updating the _id property
+
+    const _u = await userService.get(db, username);
+    if (!_u) {
+        res.status(404).json({
+            message: 'User not found'
+        });
+        return;
+    }
+
+    await userService.update(db, username, updatedValues);
+    res.status(200).json(updatedValues)
+}
+
+
+
+
+/**
+ * endpoint, to delete user
+ * @param req express request
+ * @param res express response
+ * @return {Promise<void>}
+ */
+exports.delete = async function (req, res) {
+    const username = req.params.username;
+    const db = req.app.get('db');
+    const user = await userService.get(db, username);
+    if (user != null) {
+        await userService.delete(db, username);
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({
+            message: 'User not found'
+        })
+    }
 }
--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {checkAuthorization} = require('../middlewares/auth-middleware');
+const {
+    checkAuthorization
+} = require('../middlewares/auth-middleware');
 const cors = require('cors')
 
 /*
@@ -9,39 +11,47 @@ const cors = require('cors')
 
 const authApi = require('../apis/auth-api'); //api-endpoints are loaded from separate files
 router.post('/login', authApi.login); //the function decides which request type should be accepted
-router.delete('/login', checkAuthorization(false),authApi.logout); //middlewares can be defined in parameters
+router.delete('/login', checkAuthorization(false), authApi.logout); //middlewares can be defined in parameters
 router.get('/login', authApi.isLoggedIn); //the function, which handles requests is specified as the last parameter
 
 const userApi = require('../apis/user-api');
-router.get('/user', checkAuthorization(false), userApi.getSelf);
+router.route("/user")
+    .get(checkAuthorization(false), userApi.getAll)
+    .post(checkAuthorization(true), userApi.create)
+router.route("/user/:username")
+    .get(checkAuthorization(false), userApi.getOne)
+    .put(checkAuthorization(false), userApi.update)
+    .delete(checkAuthorization(false), userApi.delete)
+
+//.get(checkAuthorization(false), userApi.getSelf)
 
 module.exports = router;
 
 const salesmanApi = require('../apis/salesman-api');
 router.route("/salesman")
-    .get( checkAuthorization(true), salesmanApi.getAll)
+    .get(checkAuthorization(true), salesmanApi.getAll)
     .post(checkAuthorization(true), salesmanApi.create)
 router.route("/salesman/:sid")
-    .get(checkAuthorization(true),salesmanApi.getBySid)
-    .put(checkAuthorization(true),salesmanApi.updateBySid)
-    .delete(checkAuthorization(true),salesmanApi.deleteBySid)
+    .get(checkAuthorization(true), salesmanApi.getBySid)
+    .put(checkAuthorization(true), salesmanApi.updateBySid)
+    .delete(checkAuthorization(true), salesmanApi.deleteBySid)
 
 const socialRecordApi = require('../apis/socialRecord-api');
 router.route("/socialrecord")
-    .get(checkAuthorization(true),socialRecordApi.getAll)
+    .get(checkAuthorization(true), socialRecordApi.getAll)
     .post(checkAuthorization(true), socialRecordApi.create)
 router.route("/socialrecord/:id")
     .get(checkAuthorization(true), socialRecordApi.getById)
     .put(checkAuthorization(true), socialRecordApi.update)
-    .delete(checkAuthorization(true),socialRecordApi.deleteById)
+    .delete(checkAuthorization(true), socialRecordApi.deleteById)
 router.route("/socialrecord/:sid/:year")
-    .get(checkAuthorization(true),socialRecordApi.getBySidAndYear)
+    .get(checkAuthorization(true), socialRecordApi.getBySidAndYear)
     .delete(checkAuthorization(true), socialRecordApi.deleteBySidAndYear)
 
 const orangeHrmApi = require('../apis/orange-hrm-api');
 router.route('/orangehrm')
-	.get(checkAuthorization(true), orangeHrmApi.synchronize)
-	.post(checkAuthorization(true), orangeHrmApi.bonusSalary)
+    .get(checkAuthorization(true), orangeHrmApi.synchronize)
+    .post(checkAuthorization(true), orangeHrmApi.bonusSalary)
 
 const productApi = require('../apis/product-api');
 router.get('/opencrx/product', checkAuthorization(true), productApi.getAll);
